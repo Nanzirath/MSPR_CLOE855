ac4e6d4d44dc25882f533b53f9891d07b131d10e
Nanzirath <155987210+Nanzirath@users.noreply.github.com>
2024-06-06 20:57:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -80,15 +80,17 @@ def fiche_nom(nom):
 
 @app.route('/enregistrer_client', methods=['POST'])
 def enregistrer_client():
+
     nom = request.form['nom']
     prenom = request.form['prenom']
+    adresse = request. form['adresse']
 
     # Connexion à la base de données
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
 
     # Exécution de la requête SQL pour insérer un nouveau client
-    cursor.execute('INSERT INTO clients (created, nom, prenom, adresse) VALUES (?, ?, ?, ?)', (1002938, nom, prenom, "ICI"))
+    cursor.execute('INSERT INTO clients (created, nom, prenom, adresse) VALUES (?, ?, ?, ?)', (1002938, nom, prenom, date))
     conn.commit()
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
